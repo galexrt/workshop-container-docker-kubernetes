a84c25a3f7a5a1dfed28a9974255bd324e71f798
Alexander Trost <galexrt@googlemail.com>
2019-04-09 19:59:41 +0200

    Fix issues with compose101 task

--- .setup/terraform/fix_servers.sh
+++ .setup/terraform/fix_servers.sh
@@ -5,7 +5,7 @@ while read -r line; do
 server="$(echo "$line" | cut -d';' -f1)"
 password="$(echo "$line" | cut -d';' -f2)"
     echo "-> $server"
-    ssh ${server} <<EOF
+    ssh -4 -o StrictHostKeyChecking=no -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null ${server} <<EOF
 sed -i -e 's/PermitRootLogin.*/PermitRootLogin yes/g' -e 's/PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
 echo -e "${password}\n${password}" | (passwd root)
--- .setup/terraform/generate_attendee_config.sh
+++ .setup/terraform/generate_attendee_config.sh
@@ -11,6 +11,8 @@ fi
 for (( i=1; i<=$1; i++ )); do
     PLAIN_PASS=2019-guug-ffg-workshop-${i}
     echo "root@k8s-c${i}-master-1.eden.run;${PLAIN_PASS}" >> servers.txt
+    echo "root@k8s-c${i}-worker-1.eden.run;${PLAIN_PASS}" >> servers.txt
+    echo "root@k8s-c${i}-worker-2.eden.run;${PLAIN_PASS}" >> servers.txt
     cat << 'EOF' | sed -e 's/_1_/_'"${i}"'_/g' -e 's/"1"/"'"${i}"'"/g' -e 's/#1/#'"${i}"'/g' -e 's/__PASS__/'"${PASS}"'/g' -e 's/__PLAIN_PASS__/'"${PLAIN_PASS}"'/g' >> attendee_clusters.tf
 # Attendee #1 - __PLAIN_PASS__
 module "hcloud_kubernetes_attendee_1_master" {
