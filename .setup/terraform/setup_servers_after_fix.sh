320d9f5e4a2420997ceee5e4e7b97827d4c79653
Alexander Trost <galexrt@googlemail.com>
2019-04-10 15:31:36 +0200

    Remove placement option from Ceph filesystem

--- .setup/terraform/setup_servers_after_fix.sh
+++ .setup/terraform/setup_servers_after_fix.sh
@@ -4,7 +4,7 @@ while read -r line; do
     server="$(echo "$line" | cut -d';' -f1)"
     password="$(echo "$line" | cut -d';' -f2)"
     echo "-> $server"
-    ssh -4 -o StrictHostKeyChecking=no -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null ${server} <<EOF
+    ssh -4 -o StrictHostKeyChecking=no -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null ${server} <<EOF &
 curl -sSL https://get.docker.com/ -o install-docker.sh
 sh install-docker.sh
 systemctl enable docker.service
@@ -12,11 +12,17 @@ systemctl start docker.service
 docker ps
 curl -sS -o /etc/sysctl.d/90-edenmal-custom.conf https://gist.githubusercontent.com/galexrt/8faa48a05bab303ec922bd89e8f7adc5/raw/63302c8d2374d198e09f7b8b9ad3cbdd31eb9061/90-edenmal-custom.conf
 sysctl -p
+dnf -y install git
 EOF
-    if echo "$server" | grep -q -- -master-; then
+done
+wait
+while read -r line; do
+    server="$(echo "$line" | cut -d';' -f1)"
+    if echo "$server" | grep -q master-; then
         continue
     fi
-    ssh -4 -o StrictHostKeyChecking=no -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null ${server} <<EOSCRIPT
+    echo "-> $server"
+    ssh -4 -o StrictHostKeyChecking=no -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null ${server} <<EOSCRIPT &
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -31,5 +37,6 @@ setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 systemctl enable --now kubelet
+mkdir -p /opt/cni/bin/ && cp /usr/libexec/cni/* /opt/cni/bin/
 EOSCRIPT
 done < servers.txt
